265acb5ebaa3c89adfc09069bc3081e7a139d968
mkirsanov <mkirsanov1@gmail.com>
2023-12-19 20:56:59 +0600

    Basic operations added

---
+++ Lesson1/Task2/Program.cs
@@ -0,0 +1,18 @@
+﻿int i1 = 5, i2 = 6;
+int sum = i1 + i2;
+Console.WriteLine(sum);
+
+long l1 = 6;
+long sum2 = i1 + l1;
+Console.WriteLine(sum2);
+
+double d1 = 5.5;
+double sum3 = i1 + d1;
+Console.WriteLine(sum3);
+
+int mult1 = i1 * i2;
+long mult2 = i1 * l1;
+double mult3 = i1 * d1;
+Console.WriteLine(mult1);
+Console.WriteLine(mult2);
+Console.WriteLine(mult3);
