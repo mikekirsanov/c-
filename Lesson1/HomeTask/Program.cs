dee8272aee07787ad029105562becab1ac8de7fd
mkirsanov <mkirsanov1@gmail.com>
2023-12-20 15:00:22 +0600

    Lesson1:HomeTask

---
+++ Lesson1/HomeTask/Program.cs
@@ -0,0 +1,7 @@
+﻿int a = 45, b = 34, c = 65, d = 93;
+int e = (a + b)/(c + d);
+Console.WriteLine(e);
+
+float af = 45, bf = 34, cf = 65, df = 93;
+float ef = (af + bf)/(cf + df);
+Console.WriteLine(ef);
