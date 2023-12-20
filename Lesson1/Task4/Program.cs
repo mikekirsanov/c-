6b2b7164588fb2ff31e930d3f53bee7961699492
mkirsanov <mkirsanov1@gmail.com>
2023-12-20 14:26:50 +0600

    Comparшson Algo added Lesson1:Task4

---
+++ Lesson1/Task4/Program.cs
@@ -0,0 +1,19 @@
+﻿int a = 6, b = 7, c = 9, d = 4, e = 10;
+int max = a;
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+Console.WriteLine(max);
