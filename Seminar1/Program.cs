71f3bb037fec845122fc21c555c699acaef24616
mkirsanov <mkirsanov1@gmail.com>
2023-12-22 00:45:50 +0600

    First exercise

---
+++ Seminar1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число");
+ int a = int.Parse(Console.ReadLine());
+ Console.WriteLine("Введите второе число");
+ int b = int.Parse(Console.ReadLine());
+ if (a == b * b)
+ {
+    Console.WriteLine("Первое число является квадратом второго.");
+ }
+ else
+ {
+    Console.WriteLine("Первое число не является квадратом второго.");
+ }
