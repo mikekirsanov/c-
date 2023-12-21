2e80998e22230bc912f372f3b8886ed1a4a42b8a
mkirsanov <mkirsanov1@gmail.com>
2023-12-22 02:14:36 +0600

    All exercises

--- Seminar1/Program.cs
+++ Seminar1/Program.cs
@@ -1,4 +1,4 @@
-﻿Console.WriteLine("Введите число");
+﻿/* Console.WriteLine("Введите число");
  int a = int.Parse(Console.ReadLine());
  Console.WriteLine("Введите второе число");
  int b = int.Parse(Console.ReadLine());
@@ -10,3 +10,22 @@
  {
     Console.WriteLine("Первое число не является квадратом второго.");
  }
+ */
+
+/* Console.WriteLine("Введите число");
+ int num = int.Parse(Console.ReadLine());
+ int negNum = -1 * num;
+ while(negNum <= num)
+{
+    Console.WriteLine(negNum);
+    negNum = negNum + 1;
+}
+*/
+
+Console.WriteLine("Введите трехзначное число");
+int num = int.Parse(Console.ReadLine());
+int num1 = num / 100;
+int num3 = num % 10;
+int result = num1 + num3;
+Console.WriteLine("Сумма первой и третьей цифр числа равна " + result); //конкатенация
+Console.WriteLine($"Сумма первой и третьей цифр числа равна {result}"); //интерполяция
