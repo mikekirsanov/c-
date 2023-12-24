1d65fc080daf7393048448f26db44b21ceff498e
mkirsanov <mkirsanov1@gmail.com>
2023-12-24 15:08:40 +0600

    Seminar1:Task1

---
+++ Seminar1/HomeTask/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+public class Answer {
+    static void CompareNumbers(int firstNumber, int secondNumber)
+    {
+        // Введите свое решение ниже
+        if(firstNumber > secondNumber)
+        {
+            Console.WriteLine($"Первое число `{firstNumber}` больше, чем второе число `{secondNumber}`");
+        }
+        else if(firstNumber < secondNumber)
+        {
+            Console.WriteLine($"Первое число `{firstNumber}` меньше, чем второе число `{secondNumber}`");
+        }
+        else
+        {
+            Console.WriteLine($"Введенные числа равны `{firstNumber}`");
+        }
+    }
+
+
+  // Не удаляйте и не меняйте метод Main! 
+    static public void Main(string[] args) {
+        int firstNumber, secondNumber;
+
+        if (args.Length >= 2) {
+            firstNumber = int.Parse(args[0]);
+            secondNumber = int.Parse(args[1]);
+        } else {
+           // Здесь вы можете поменять значения для отправки кода на Выполнение
+            firstNumber = 5;
+            secondNumber = 6;
+        }
+
+        // Не удаляйте строки ниже
+        CompareNumbers(firstNumber, secondNumber);
+    }
+}
