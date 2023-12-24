9863b75b5a90e06dc9f121f875dc2f1104a67910
mkirsanov <mkirsanov1@gmail.com>
2023-12-24 17:28:21 +0600

    Seminar1:Task3

--- Seminar1/HomeTask/Program.cs
+++ Seminar1/HomeTask/Program.cs
@@ -36,7 +36,7 @@ public class Answer {
         CompareNumbers(firstNumber, secondNumber);
     }
 }
-*/
+
 
 using System;
 
@@ -80,5 +80,39 @@ public class Answer
         // Не удаляйте строки ниже
         int result = Findresult(a, b, c);
         System.Console.WriteLine($"{result}");
+    }
+}
+*/
+
+using System;
+
+class Answer {
+    static void CheckIfEven(int number)
+    {
+      // Введите свое решение ниже
+        if(number % 2 == 0)
+        {
+            Console.WriteLine($"Число `{number}` чётное");
+        }
+        else
+        {
+            Console.WriteLine($"Число `{number}` нечётное");
+        }
+    }
+  
+
+  // Не удаляйте и не меняйте метод Main! 
+    static public void Main(string[] args) {
+        int number;
+
+        if (args.Length >= 1) {
+            number = int.Parse(args[0]);
+        } else {
+           // Здесь вы можете поменять значения для отправки кода на Выполнение
+            number = 6;
+        }
+
+        // Не удаляйте строки ниже
+        CheckIfEven(number);
     }
 }
