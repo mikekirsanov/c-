25a4192044762d59ddb98f3a3c0f9b594cef7820
mkirsanov <mkirsanov1@gmail.com>
2023-12-25 23:21:48 +0600

    Seminar2:Task1

---
+++ Seminar2/Task1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите трехзначное число.");
+int num = Convert.ToInt32(Console.ReadLine());
+//int num2 = int.Parse(Console.ReadLine());
+if(num > 99 && num < 1000)
+{
+    int num1 = num / 100;
+    int num3 = num % 10;
+    int result = num1 * 10 + num3;
+    Console.WriteLine(result);
+}
