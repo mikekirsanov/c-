01cf847fb5760fcebd891c1488b94fefb9b7b7bf
mkirsanov <mkirsanov1@gmail.com>
2023-12-29 19:13:50 +0600

    Lesson3:Task1

---
+++ Lesson3/Task1/Program.cs
@@ -0,0 +1,10 @@
+﻿double CalculteFormula(int a, int b, int c, int d)
+{
+    double numerator = a * b;
+    int denominator = c + d;
+    double result = numerator / denominator;
+    return result;
+}
+
+double result = CalculteFormula(34, 345, 323, 67);
+Console.Write(result);
