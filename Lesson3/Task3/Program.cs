462f2191cb307ca6a9be4528034eea082a3aebe8
mkirsanov <mkirsanov1@gmail.com>
2023-12-30 20:55:57 +0600

    Lesson3:Task3

---
+++ Lesson3/Task3/Program.cs
@@ -0,0 +1,22 @@
+﻿void ZeroEvenElements(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            arr[i] = 0;
+        }
+    }
+}
+
+void PrintArray(int[] array)
+{
+    foreach(int e in array)
+    {
+    Console.Write($"{e} ");
+    }
+}
+
+int[] array = {23, 34, 45, 23, 67, 34};
+ZeroEvenElements(array);
+PrintArray(array);
