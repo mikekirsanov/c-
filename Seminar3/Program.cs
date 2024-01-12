25beca8d16a7b76cec2d478dda6926c9f3986710
mkirsanov <mkirsanov1@gmail.com>
2024-01-12 22:55:48 +0600

    added Seminar3:Task1

---
+++ Seminar3/Program.cs
@@ -0,0 +1,17 @@
+﻿int [] array = new int [10];
+
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+string find= "Нет";
+
+for (int i = 0; i < array.Length; i++)
+{
+array[i] = new Random().Next(0,100);
+Console.Write(array[i]+ " ");
+
+if(array[i]== num)
+{
+find = "Да";
+}
+}
+Console.WriteLine(find);
