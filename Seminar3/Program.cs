a16d5bfc39516fa8feea7c1c35bb334632067c09
mkirsanov <mkirsanov1@gmail.com>
2024-01-12 23:05:07 +0600

    added Seminar3:Task2

--- Seminar3/Program.cs
+++ Seminar3/Program.cs
@@ -1,4 +1,4 @@
-﻿int [] array = new int [10];
+﻿/*int [] array = new int [10];
 
 Console.WriteLine("Введите число");
 int num = Convert.ToInt32(Console.ReadLine());
@@ -15,3 +15,28 @@ find = "Да";
 }
 }
 Console.WriteLine(find);
+*/
+// Задайте массив из 10 элементов, заполненный числами из
+// промежутка [-10, 10]. Замените отрицательные элементы на
+// положительные, а положительные на отрицательные.
+// Пример
+// [1 -5 6]
+// => [-1 5 -6]
+
+int sizeArray = 10;
+
+int [] arrray = new int [sizeArray];
+
+for (int i = 0; i < arrray.Length; i++)
+{
+arrray[i] = new Random().Next(-10,11);
+Console.Write(arrray[i]+ " ");
+}
+
+System.Console.WriteLine();
+
+for (int i = 0; i < arrray.Length; i++)
+{
+arrray[i] = -1 * arrray[i];
+Console.Write(arrray[i]+ " ");
+}
