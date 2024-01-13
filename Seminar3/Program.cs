dd9f2b568451e789bc9feff1a26401acc584ca6c
mkirsanov <mkirsanov1@gmail.com>
2024-01-13 20:03:04 +0600

    added Seminar3:Task4

--- Seminar3/Program.cs
+++ Seminar3/Program.cs
@@ -48,7 +48,7 @@ Console.Write([i]+ " ");
 // [1 3 2 4 2 3] => [3 6 8]
 // [2 3 1 7 5 6 3] => [6 18 5] (элемент 7 не имеет пары)
 
-System.Console.WriteLine("Введите размер массива.");
+/*System.Console.WriteLine("Введите размер массива.");
 int sizeArray = Convert.ToInt32(Console.ReadLine());
 
 int[] array = new int[sizeArray];
@@ -66,4 +66,23 @@ for (int i = 0; i < array2.Length; i++)
 {
     array2[i] = array[i] * array[array.Length - 1 - i];
     System.Console.Write(array2[i] + "\t");
+}*/
+
+// Дано натуральное трёхзначное число. Создайте массив, состоящий из
+// цифр этого числа. Младший разряд числа должен располагаться на 0-
+// м индексе массива, старший – на 2-м.
+// Пример
+// 456 => [6 5 4]
+// 781 => [1 8 7]
+
+System.Console.WriteLine("Введите трехзначное число.");
+int num = Convert.ToInt32(Console.ReadLine());
+int sizeArray = 3;
+
+int[] array = new int[sizeArray];
+
+for (int i = 0; num > 0; i++, num /= 10)
+{
+    array[i] = num % 10;
+    Console.Write(array[i] + " ");
 }
