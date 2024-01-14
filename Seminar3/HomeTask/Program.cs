d3624869ec88fd6499ed24f5dfe60afa075ee719
mkirsanov <mkirsanov1@gmail.com>
2024-01-14 22:58:03 +0600

    Task2

--- Seminar3/HomeTask/Program.cs
+++ Seminar3/HomeTask/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿/*using System;
 using System.Linq;
 
 //Тело класса будет написано студентом. Класс обязан иметь статический метод PrintResult()
@@ -53,6 +53,62 @@ class Answer
         } else {
            // Если аргументов на входе нет
             array = new int[] {1, 5, 10, 20, 30, 40, 99, 4, 90, 3}; // Создание массива
+        }
+        UserInputToCompileForTest.PrintResult(array);
+    }
+}*/
+
+using System;
+using System.Linq;
+
+//Тело класса будет написано студентом. Класс обязан иметь статический метод PrintResult()
+class UserInputToCompileForTest
+{ 
+    // Подсчет количества четных элементов массива
+    // numbers - массив, в котором ведется подсчет
+    public static int CountEvenItems(int[] numbers)
+    {
+        //Напишите свое решение здесь
+        int result = 0; 
+        for(int i=0; i < numbers.Length; i++)
+        {
+          if(numbers[i]%2 == 0)
+          {
+            result += 1;
+          }
+        }
+      return result;
+    }
+    
+    public static void PrintResult(int[] array)
+    {
+        //Напишите свое решение здесь
+      System.Console.WriteLine(CountEvenItems(array));
+    }
+}
+
+//Не удаляйте и не меняйте класс Answer!
+class Answer
+{
+    public static void Main(string[] args)
+    {
+        int[] array;
+        
+
+        if (args.Length >= 1) {
+            // Объединяем все аргументы командной строки в одну строку
+            string joinedArgs = string.Join(", ", args);
+
+            // Разделяем строку по запятой с пробелом и преобразуем в массив целых чисел
+            array = joinedArgs.Split(", ")
+                                  .Select(int.Parse)
+                                  .ToArray();
+            
+            // Теперь arr содержит преобразованные в целые числа из командной строки
+        
+        } else {
+           // Если аргументов на входе нет
+            array = new int[] {2, 5, 2, 3, 6, 3, 7, 8, 2, 4}; // Создание массива
         }
         UserInputToCompileForTest.PrintResult(array);
     }
