3a2b1a65cc4aedb6cd1aa86417470db2e1724f15
mkirsanov <mkirsanov1@gmail.com>
2024-01-21 01:36:13 +0600

    Seminar4:HomeTask3

---
+++ Seminar4/HomeTask/Task3/Program.cs
@@ -0,0 +1,42 @@
+﻿//Задача 3: Напишите программу, которая перевернёт одномерный массив (первый элемент станет последним, второй – предпоследним и т.д.)
+
+using System;
+
+int[] CreateMassive(int size)
+{
+    Random random = new Random();
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = random.Next(100, 1000);
+    }
+    return array;
+}
+
+int[] ChangeMassive(int[] array)
+{
+    for (int i = 0; i < array.Length / 2; i++)
+    {
+        int temp = array[i];
+        array[i] = array[array.Length - 1 - i];
+        array[array.Length - 1 - i] = temp;
+    }
+    return array;
+}
+
+void PrintMassive(int[] array)
+{
+    foreach (int e in array)
+    {
+        Console.Write(e + " ");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine("Введите размер массива.");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] array = CreateMassive(size);
+PrintMassive(array);
+ChangeMassive(array);
+Console.WriteLine("Перевернутый массив:");
+PrintMassive(array);
