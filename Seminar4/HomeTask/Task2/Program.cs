1888032374b4139c9db9e7a0db15420724368d3b
mkirsanov <mkirsanov1@gmail.com>
2024-01-21 01:00:38 +0600

    Seminar4:HomeTask2

---
+++ Seminar4/HomeTask/Task2/Program.cs
@@ -0,0 +1,38 @@
+﻿//Задайте массив заполненный случайными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+using System;
+
+int[] CreateMassive(int size)
+{
+    Random random = new Random();
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = random.Next(100, 1000);
+    }
+    return array;
+}
+
+void PrintMassive(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+int EvenNumbersCount(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0) count += 1;
+    }
+    return count;
+}
+
+Console.WriteLine("Введите размер массива.");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] array = CreateMassive(size);
+PrintMassive(array);
+Console.WriteLine("Количество четных чисел в массиве равно " + EvenNumbersCount(array));
