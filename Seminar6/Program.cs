2a2c8d265255dd982ed4f7304628234a480b2f17
mkirsanov <mkirsanov1@gmail.com>
2024-01-22 23:26:58 +0600

    Seminar6:Task1

---
+++ Seminar6/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задайте массив символов (тип char []). Создайте строку из
+// символов этого массива.
+// Указание
+// Конструктор строки вида string(char []) не использовать.
+// Пример
+// [‘a’, ‘b’, ‘c’, ‘d’] => “abcd”
+
+
+char [] array = new char[]{'t','y','3','r','$',};
+string str = "";
+
+for (int i = 0; i < array.Length; i++)
+{
+    str = str + array[i];
+}
+
+System.Console.WriteLine(str);
