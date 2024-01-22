b6885110f96f261eec0108905c2d46958c86717c
mkirsanov <mkirsanov1@gmail.com>
2024-01-22 23:40:28 +0600

    Seminar6:Task2

--- Seminar6/Program.cs
+++ Seminar6/Program.cs
@@ -6,7 +6,7 @@
 // [‘a’, ‘b’, ‘c’, ‘d’] => “abcd”
 
 
-char [] array = new char[]{'t','y','3','r','$',};
+/*char [] array = new char[]{'t','y','3','r','$',};
 string str = "";
 
 for (int i = 0; i < array.Length; i++)
@@ -15,3 +15,20 @@ for (int i = 0; i < array.Length; i++)
 }
 
 System.Console.WriteLine(str);
+*/
+// На основе символов строки (тип string) сформировать массив
+// символов (тип char[]). Вывести массив на экран.
+// Указание
+// Метод строки ToCharArray() не использовать.
+// Пример
+// “Hello!” => [‘H’, ‘e’, ‘l’, ‘l’, ‘o’, ‘!’ ] 
+
+System.Console.WriteLine("Введите строчку");
+string str = Console.ReadLine();
+char [] array = new char[str.Length];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = str[i];
+    System.Console.Write(array[i] + "");
+}
