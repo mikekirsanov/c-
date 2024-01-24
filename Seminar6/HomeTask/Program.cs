b9cf3da246d6ba08d89a4b570fee854e07fb6036
mkirsanov <mkirsanov1@gmail.com>
2024-01-24 07:33:18 +0600

    Seminar6:HomeTask1

---
+++ Seminar6/HomeTask/Program.cs
@@ -0,0 +1,13 @@
+﻿//Задача 1: Задайте двумерный массив символов (тип char [,]). Создать строку из символов этого массива.
+
+using System;
+char [,] array = {{'H', 'e', 'l', 'l', 'o', ','}, {' ', 'w', 'o', 'r', 'l', 'd'}};
+string str = "";
+for(int i = 0; i < array.GetLength(0); i++)
+{
+    for(int j = 0; j < array.GetLength(1); j++)
+    {
+        str += array[i,j];
+    }
+}
+Console.WriteLine(str);
