e46217a9bcc0c023bf5718c2acf6d592b5e55bbe
mkirsanov <mkirsanov1@gmail.com>
2024-01-25 19:50:42 +0600

    Seminar6:HomeTask2

--- Seminar6/HomeTask/Program.cs
+++ Seminar6/HomeTask/Program.cs
@@ -1,6 +1,6 @@
 ﻿//Задача 1: Задайте двумерный массив символов (тип char [,]). Создать строку из символов этого массива.
 
-using System;
+/*using System;
 char [,] array = {{'H', 'e', 'l', 'l', 'o', ','}, {' ', 'w', 'o', 'r', 'l', 'd'}};
 string str = "";
 for(int i = 0; i < array.GetLength(0); i++)
@@ -11,3 +11,23 @@ for(int i = 0; i < array.GetLength(0); i++)
     }
 }
 Console.WriteLine(str);
+*/
+
+//Задача 2: Задайте строку, содержащую латинские буквы в обоих регистрах. Сформируйте строку, в которой все заглавные буквы заменены на строчные.
+
+using System;
+Random rnd = new Random();
+Console.WriteLine("Введите размер строки.");
+int num = Convert.ToInt32(Console.ReadLine());
+string str = "";
+for(int i = 0; i < num; i++)
+{
+    int numchar1 = rnd.Next(65,91);
+    int numchar2 = rnd.Next(97,123);
+    int alfa = rnd.Next(1,3);
+    if(alfa == 1) str += (char)numchar1;
+    else str += (char)numchar2;
+}
+Console.WriteLine(str);
+string str2 = str.ToLower();
+Console.WriteLine(str2);
