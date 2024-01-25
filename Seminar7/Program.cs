90a6d14c1c286aa5a8b5c876ab291822e4710c7b
mkirsanov <mkirsanov1@gmail.com>
2024-01-26 00:25:28 +0600

    Seminar7:Task1

---
+++ Seminar7/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая будет принимать на вход число и
+// возвращать сумму его цифр.
+// Указание
+// Использовать рекурсию.
+// Пример
+// 123 => 6
+// 63 => 9
+
+int num = int.Parse(Console.ReadLine());  //  123
+
+int GetSumNum(int newNum) // 123
+{
+    if(newNum==0)
+    {
+        return 0;
+    }
+
+    return (newNum%10 + GetSumNum(newNum/10));   // 3 + 2+1 +0
+}
+System.Console.WriteLine(GetSumNum(num));
+
