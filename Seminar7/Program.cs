15f7df17fe146a49b3f377a163c89c2ed61e8bc7
mkirsanov <mkirsanov1@gmail.com>
2024-01-26 00:46:56 +0600

    Seminar7:Task2

--- Seminar7/Program.cs
+++ Seminar7/Program.cs
@@ -6,7 +6,7 @@
 // 123 => 6
 // 63 => 9
 
-int num = int.Parse(Console.ReadLine());  //  123
+/*int num = int.Parse(Console.ReadLine());  //  123
 
 int GetSumNum(int newNum) // 123
 {
@@ -19,3 +19,29 @@ int GetSumNum(int newNum) // 123
 }
 System.Console.WriteLine(GetSumNum(num));
 
+*/
+
+// Задайте значение N. Напишите программу, которая выведет
+// все натуральные числа в промежутке от 1 до N.
+// Указание
+// Использовать рекурсию. Не использовать цикл.
+// Пример
+// N=5 => 1 2 3 4 5
+
+System.Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+int firstElement = 1;
+
+PrintNumbers(firstElement,num);
+
+void PrintNumbers(int firstNum, int endNum)
+{
+    if(firstNum> endNum)
+    {
+        return;
+    }
+
+    System.Console.WriteLine(firstNum);
+
+    PrintNumbers(firstNum+1, endNum);
+}
