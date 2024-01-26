0b3075c511d40d89c5214c6c7acc31b7851312db
mkirsanov <mkirsanov1@gmail.com>
2024-01-26 21:37:47 +0600

    Seminar7:Task1

---
+++ Seminar7/HomeTask/Task1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 1: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N. Использовать рекурсию, не использовать циклы.
+
+using System;
+Console.WriteLine("Введите первое число.");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число.");
+int N = Convert.ToInt32(Console.ReadLine());
+int min = M;
+int max = N;
+if(M > N)
+{
+    min = N;
+    max = M;
+}
+
+PrintRangeOfNumbers(min, max);
+
+void PrintRangeOfNumbers(int Min, int Max)
+{
+    if(Min > Max)
+    {
+        return;
+    }
+
+    Console.Write(Min + "\t");
+    PrintRangeOfNumbers(Min+1, Max);
+}
