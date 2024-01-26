3115d14de3b574212f0228828ea99d6fa75c4bd2
mkirsanov <mkirsanov1@gmail.com>
2024-01-26 21:51:23 +0600

    Seminar7:Task1 change

--- Seminar7/HomeTask/Task1/Program.cs
+++ Seminar7/HomeTask/Task1/Program.cs
@@ -5,23 +5,28 @@ Console.WriteLine("Введите первое число.");
 int M = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число.");
 int N = Convert.ToInt32(Console.ReadLine());
-int min = M;
-int max = N;
-if(M > N)
-{
-    min = N;
-    max = M;
-}
 
-PrintRangeOfNumbers(min, max);
+if (M < N) PrintRangeOfNumbers(M, N);
+else PrintReverseRangeOfNumbers(M, N);
 
 void PrintRangeOfNumbers(int Min, int Max)
 {
-    if(Min > Max)
+    if (Min > Max)
     {
         return;
     }
 
     Console.Write(Min + "\t");
-    PrintRangeOfNumbers(Min+1, Max);
+    PrintRangeOfNumbers(Min + 1, Max);
+}
+
+void PrintReverseRangeOfNumbers(int Max, int Min)
+{
+    if (Max < Min)
+    {
+        return;
+    }
+
+    Console.Write(Max + "\t");
+    PrintReverseRangeOfNumbers(Max - 1, Min);
 }
