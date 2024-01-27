013f26181c111328261a56fe41425f2e43e37479
mkirsanov <mkirsanov1@gmail.com>
2024-01-27 23:20:30 +0600

    Seminar7:HomeTask3

---
+++ Seminar7/HomeTask/Task3/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 3: Задайте произвольный массив. Выведете его элементы, начиная с конца. Использовать рекурсию, не использовать циклы.
+using System;
+Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+Random rnd = new Random();
+
+Console.WriteLine("Новый массив:");
+int[] array = CreateArray(size);
+Console.WriteLine();
+
+Console.WriteLine("Перевернутый массив:");
+PrintReverseArray(array, size);
+
+int[] CreateArray(int size)
+{
+   int[] array = new int[size];
+   for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(1, 1000);
+        Console.Write($"{array[i]}\t");
+    }
+    return array;
+}
+
+void PrintReverseArray(int[] array, int index)
+{
+{
+    if(index < 1)
+    return;
+}
+Console.Write(array[index - 1] + "\t");
+PrintReverseArray(array, index - 1);
+}
